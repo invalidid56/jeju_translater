0df5d8ba8b5b3f50fd70025bbeb4e43f7eec61e2
invalidID1956 <karl2003@naver.com>
2019-11-05 22:25:52 +0900

    Pos Tagger Debugged
    - 포스태거 교체: 코모란->OKT (처리길이 문제)
    - 엑셀 파일 읽어들일 때 오류 발생(한셀 호환성? 확인요)
    - 엑셀 버전 낮춰 저장하면 해결됨, 문제 있음.
    - 이외에 정상 작동 확인됨, 코퍼스 생성 확인
    - 품사 구분 슬래쉬 유지? 공백이 나으려나?

--- generate_corpus.py
+++ generate_corpus.py
@@ -6,38 +6,39 @@ import os
 import sys
 import openpyxl
 
-from konlpy.tag import Komoran
+from konlpy.tag import Okt
 
 
 def main(corpora, output):
     filelist = os.listdir(corpora)
-    tagger = Komoran()
-    for file in filelist:
-        book = openpyxl.load_workbook(file)
-        sheet = book.get_sheet_by_name["Sheet1"]
+    tagger = Okt()
 
-        tagged = (bool(sheet.rows[0][2].value) or bool(sheet.rows[0][3].value))
+    for file in filelist:
+        book = openpyxl.load_workbook(os.path.join(corpora, file))
+        sheet = book.get_sheet_by_name("Sheet1")
+
+        tagged = (bool(sheet.cell(row=1, column=3).value) and bool(sheet.cell(row=1, column=4).value))
 
         if not tagged:
             for sample in sheet.rows:
                 index = sample[0].row
 
                 stan = sample[0].value
-                pos_stan = tagger.morphs(stan)
+                pos_stan = '/'.join(tagger.morphs(stan))
                 jeju = sample[1].value
-                pos_jeju = tagger.morphs(jeju)
+                pos_jeju = '/'.join(tagger.morphs(jeju))
 
-                sheet.cell(row=index, column=2).value = pos_stan
-                sheet.cell(row=index, column=3).value = pos_jeju
+                sheet.cell(row=index, column=3).value = pos_stan
+                sheet.cell(row=index, column=4).value = pos_jeju
 
-            book.save(file)
+            book.save(os.path.join(corpora, file))
 
         filename = file[:file.find('.')]
-        output_dir = os.path.join(output, filename, '.txt')    # Exception: Output Dir not Exists
+        output_dir = os.path.join(output, filename + '.txt')    # Exception: Output Dir not Exists
         output_file = open(output_dir, 'w')
 
         for sample in sheet.rows:
-            line = '\t'.join([s.value for s in sample[:4]]) + '\n'  # Exception: s.value can be no string
+            line = '\t'.join([s.value for s in sample[:5]]) + '\n'  # Exception: s.value can be no string
             output_file.write(line)
 
         output_file.close()
